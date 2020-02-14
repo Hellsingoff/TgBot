8cf9cdb989459e2fc573a2a5a1cfe67951fefcd8
Hellsingoff <hellsingoff@gmail.com>
2020-02-14 19:18:10 +0500

    except long msg

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def send_message(user_id: int, text: str, disable_notif: bool=False):
         await sleep(0.1)
     msg_counter += 1
     try:
-        await bot.send_message(user_id, text[:4096], 
+        await bot.send_message(user_id, text, 
                                disable_notification=disable_notif)
     except exceptions.BotBlocked:
         log.error(f"Target [ID:{user_id}]: blocked by user")
@@ -53,6 +53,13 @@ async def send_message(user_id: int, text: str, disable_notif: bool=False):
         return await send_message(user_id, text, disable_notif)
     except exceptions.UserDeactivated:
         log.error(f"Target [ID:{user_id}]: user is deactivated")
+    except exceptions.MessageIsTooLong:
+        log.error(f"Target [ID:{user_id}]: msg len {len(text)}")
+        start_char = 0
+        while start_char <= len(text):
+            await send_message(user_id, text[start_char:start_char + 4096],
+                               disable_notification=disable_notif)
+            start_char += 4096
     except exceptions.TelegramAPIError:
         log.exception(f"Target [ID:{user_id}]: failed")
     else:
