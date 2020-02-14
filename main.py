7656850659071eaedb97e1f9b590d9a961fd75b7
Hellsingoff <hellsingoff@gmail.com>
2020-02-15 01:35:53 +0500

    Update main.py

--- main.py
+++ main.py
@@ -175,6 +175,6 @@ if __name__ == '__main__':
         webhook_path=WEBHOOK_PATH,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
-        skip_updates=False,
+        skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT)
